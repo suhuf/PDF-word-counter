e30501c341d233fcbb8ad1d91bae125d4f872e11
suhuf <105312929+suhuf@users.noreply.github.com>
2023-09-15 02:39:18 -0400

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,13 +1,13 @@
 import requests
 import os
 
+
 set2 = 0
 
 set = 0
 
 y_response = ["Yes", "ye", "y", "yes"]
 n_response = ["No", "no", "n", "na"]
-
 
 while set2 == 0:
 
@@ -20,6 +20,7 @@ while set2 == 0:
     else:
         print(r"You are not in C:\Users\seca\Documents\Programming_Etc")  # Change.
 
+
 while set == 0:
 
     chc = input("Would you like to list directories? Yes/No ")
@@ -27,24 +28,39 @@ while set == 0:
     if chc not in y_response and chc not in n_response:
         print("Your input is invalid, requiring a yes or no. ")
 
-    else:
-        pass
-
     if chc in y_response:
-        set += 1
         print(r"Listing directory of " + (os.getcwd()))
         d_list = (os.listdir())
-
         print("\n", *d_list, sep = "\n")
+        set =+ 1
 
     if chc in n_response:
         print("Cancelling") # Create loop back menu
-        set += 1
 
         break
 
 
+while set == 1:
 
-    input("Would you like to extract word count of ... " + str() )
+    chc2 = input("\n" "Would you like to extract word count of ... ") # len
 
-    continue
+    a_list = []
+
+    if chc2 in y_response:
+        for i in d_list:
+            ext_n = os.path.splitext(i)[-1]
+            if ext_n == ".pdf":
+                a_list.append(i)
+
+            else:
+                print(r"This Item: " + str(i) + " will not be parsed as it is not a .pdf file."
+                                                " (This may be an extension naming issue)" "\n")
+
+        print("\n" "These are your .PDFs primed for analysis: " + "\n", *a_list, sep = "\n")
+
+    if chc2 in n_response:
+        print("Cancelling")  # Create loop back menu
+
+        break
+
+
